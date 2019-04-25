dc2ea6178b1de5182339df34533ba8651d202fd9
aluno <aluno@lab3q105-p5qc01.ufu.br>
2019-04-25 12:16:34 -0300

    Lab01 - JavaScript

---
+++ respJS_001.js
@@ -0,0 +1,33 @@
+function Q1() {
+    console.log('Q1');
+    var x = Number.parseInt(document.getElementById('x').value);
+    var y = Number.parseInt(document.getElementById('y').value);
+    // Segue aqui seu código
+    console.log('X = ' + x);
+    console.log('Y = ' + y);
+    console.log('Quociente = ' + Math.floor(x/y));
+    console.log('Resto = ' + (x%y));
+
+
+}
+function Q2() {
+    console.log('Q2');
+    var x = Number.parseFloat(document.getElementById('b').value);
+    var y = Number.parseFloat(document.getElementById('c').value);
+    // Segue aqui seu código
+    console.log('X = ' + x);
+    console.log('Y = ' + y);
+    console.log('Hipotenusa = ' + Math.sqrt((x * x) + (y * y)));
+
+}
+function Q3() {
+    console.log('Q3');
+    var aux = document.getElementById('profissoes');
+    var profissao = aux.options[aux.selectedIndex].text;
+    // Segue aqui seu código
+    if (profissao.endsWith('a')
+        {console.log 'feminino'}
+
+
+
+}
